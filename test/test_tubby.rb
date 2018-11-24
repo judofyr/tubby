d03b120f1472b3c289f75d6cbfc91ec869e68330
Magnus Holm <judofyr@gmail.com>
2018-11-24 12:01:21 +0000

    Add test for attrs

--- test/test_tubby.rb
+++ test/test_tubby.rb
@@ -75,5 +75,13 @@ class TestTubby < Minitest::Test
       }.to_s
     end
   end
+
+  def test_attrs
+    tmpl = Tubby.new { |t|
+      t.h1(a: false, b: nil, c: "", d: 123, e: [], f: [nil, 1, false, 2], g: true)
+    }
+
+    assert_equal '<h1 c="" d="123" e="" f="1 2" g></h1>', tmpl.to_s
+  end
 end
 
